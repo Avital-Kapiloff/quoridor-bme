725dde3e16cdcc753534684984efb9e7cc2863cd
Haroun.Minhas <Haroun.Minhas@40f34c9e-2841-11df-b6db-4758a1b4a51e>
2010-03-11 01:33:51 +0000

    raar dink

--- src/domein/Spelbord.java
+++ src/domein/Spelbord.java
@@ -3,13 +3,11 @@ package domein;
 
 public class Spelbord {
 
-	private int rij;
-	private int kolom;
-	public int[][] spelbordVeld;
+	public int[][] bord;
 	
 	public Spelbord(int rij,int kolom){
 		int dimensie=(rij*2)-1;
-		spelbordVeld= new int[dimensie][dimensie];
+		bord= new int[dimensie][dimensie];
 	}
 	
 
